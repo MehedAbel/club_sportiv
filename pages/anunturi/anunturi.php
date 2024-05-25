088f5d9ef680f1cdae79b6305ff8aa3afb6e1b58
Abel <111386807+abel325@users.noreply.github.com>
2024-05-25 09:40:07 +0300

    create project structure

---
+++ pages/anunturi/anunturi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="anunturi.css">
+    <title>Anunturi</title>
+</head>
+<body>
+<div class="container">
+        <?php include '../../includes/nav.php' ?>
+        <div class="container__content">
+            <h1>Anunturi</h1>
+        </div>
+    </div>
+</body>
+</html>
---
+++ pages/contact/contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="contact.css">
+    <title>Contact</title>
+</head>
+<body>
+<div class="container">
+        <?php include '../../includes/nav.php' ?>
+        <div class="container__content">
+            <h1>Contact</h1>
+        </div>
+    </div>
+</body>
+</html>
